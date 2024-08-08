8966b254861f9226138e3f0b7391b3a4fe032937
dcjh <140426836+dcjh@users.noreply.github.com>
2024-08-08 17:08:08 +0800

    created pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import './pages/index';
+import { Checkout, CreateData, DeleteData, ShowData, Home, EditData } from './pages/index';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/data/details/:id' element={<ShowData />} />
+        <Route path='/checkout/user' element={<Checkout />} />
+        <Route path='/data/create' element={<CreateData />} />
+        <Route path='/data/delete/:id' element={<DeleteData />} />
+        <Route path='/data/edit/:id' element={<EditData />} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,7 @@
+export {default as Checkout} from "./Checkout";
+export {default as CreateData} from "./CreateData";
+export {default as DeleteData} from "./DeleteData";
+export {default as EditData} from "./EditData";
+export {default as Home} from "./Home";
+export {default as ShowData} from "./ShowData";
+
